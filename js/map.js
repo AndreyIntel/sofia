785326b9aa269ffcf29cc3f9c62838b750043992
Andrey <andrey@aura.diamonds>
2022-08-08 16:49:35 +0300

    del js

--- js/map.js
+++
@@ -1,8 +0,0 @@
-let map = document.querySelector ('.map-iframe'); 
-let maplistner = function (e) { 
-    let frame = document.createElement ('iframe'); 
-    frame.src = this.getAttribute ('data-src');  
-    map.appendChild (frame); 
-    map.removeEventListener ("mouseover", maplistner); 
-}; 
-map.addEventListener ('mouseover', maplistner);  
