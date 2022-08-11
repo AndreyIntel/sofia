e5c4eea78420b6dd77e042ee1f7ff70ae2aba8bd
Andrey <andrey@aura.diamonds>
2022-08-11 16:16:58 +0300

    mobile start

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function() {
-    const mediaQuery = window.matchMedia( '( min-width: 1025px )' )
+    const mediaQuery = window.matchMedia( '( min-width: 1201px )' )
     const navArea = document.querySelector("#header-content");
     if (mediaQuery.matches) {
         if (window.pageYOffset > 0) navArea.classList.add("is-sticky");
