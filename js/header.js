32d7f6ab1e61058888977610e79d3a4a3ee2541a
Andrey <andrey@aura.diamonds>
2022-08-11 13:49:00 +0300

    scroll animation

---
+++ js/header.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", function() {
+    const mediaQuery = window.matchMedia( '( min-width: 1025px )' )
+    const navArea = document.querySelector("#header-content");
+    if (mediaQuery.matches) {
+        if (window.pageYOffset > 0) navArea.classList.add("is-sticky");
+        else navArea.classList.remove("is-sticky");
+    }
+});
